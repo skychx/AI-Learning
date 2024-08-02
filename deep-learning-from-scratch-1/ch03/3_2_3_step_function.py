213653dfde4f0e9d423b0a91b91c5d2226790411
skychx <skychx@hotmail.com>
2024-08-02 17:06:05 +0800

    review: deep-learning-from-scratch-1 ch02 ch03

--- deep-learning-from-scratch-1/ch03/3_2_3_step_function.py
+++ deep-learning-from-scratch-1/ch03/3_2_3_step_function.py
@@ -2,7 +2,10 @@ import numpy as np
 import matplotlib.pylab as plt
 
 def step_function(x):
-  return np.array(x > 0, dtype=int)
+    """
+    阶跃函数, x 大于 0, 返回 1; 否则返回 0
+    """
+    return np.array(x > 0, dtype=int)
 
 x = np.arange(-5.0, 5.0, 0.1)
 y = step_function(x)
--- deep-learning-from-scratch-1/ch03/3_2_7_rulu.py
+++ deep-learning-from-scratch-1/ch03/3_2_7_rulu.py
@@ -3,7 +3,11 @@ import numpy as np
 import matplotlib.pylab as plt
 
 def relu(x):
-  return np.maximum(0, x)
+    """
+    ReLU(Rectified Linear Unit) 函数
+    输入大于 0 时，直接输出该值; 在输入小于等于 0 时，输出 0 
+    """
+    return np.maximum(0, x)
 
 x = np.arange(-5.0, 5.0, 0.1)
 y = relu(x)
